9800afd690af1184915c12543759cf1118ce65d0
Eas <me@eas.web.id>
2020-09-01 13:20:04 +0700

    add UserId to Todos table

---
+++ server/migrations/20200901043018-add-column-to-todo.js
@@ -0,0 +1,29 @@
+'use strict';
+
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    /**
+     * Add altering commands here.
+     *
+     * Example:
+     * await queryInterface.createTable('users', { id: Sequelize.INTEGER });
+     */
+    await queryInterface.addColumn('Todos', 'UserId', {
+      type: Sequelize.INTEGER,
+      references: {
+        model: 'Users',
+        deferrable: Sequelize.Deferrable.INITIALLY_IMMEDIATE,
+        key: 'id'
+      }
+    })
+  },
+
+  down: async (queryInterface, Sequelize) => {
+    /**
+     * Add reverting commands here.
+     *
+     * Example:
+     * await queryInterface.dropTable('users');
+     */
+  }
+};
