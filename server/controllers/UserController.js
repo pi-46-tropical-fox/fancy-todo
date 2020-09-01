42c727541d99fd40a58b746762dbc2885ada7cdf
bryandaniswara <bryan.daniswara@gmail.com>
2020-09-01 21:21:21 +0700

    Error handler and more validation

--- server/controllers/UserController.js
+++ server/controllers/UserController.js
@@ -3,29 +3,18 @@ const bcrypt = require('bcryptjs');
 const { generateToken } = require('../helpers/jwt');
 
 class UserController {
-	static async register(req, res) {
+	static async register(req, res, next) {
 		const { email, password } = req.body;
 		try {
 			const user = await User.create({ email, password });
 
 			return res.status(201).json({ id: user.id, email: user.email });
 		} catch (error) {
-			if ((error.name = 'SequelizeValidationError')) {
-				return res.status(400).json({
-					validation_errors: error.errors.map(err => {
-						return {
-							name: err.validatorKey,
-							message: err.message,
-						};
-					}),
-				});
-			} else {
-				return res.status(500).json({ message: 'Internal server error' });
-			}
+			return next(error);
 		}
 	}
 
-	static async login(req, res) {
+	static async login(req, res, next) {
 		try {
 			const { email, password } = req.body;
 			const user = await User.findOne({ where: { email } });
@@ -46,16 +35,10 @@ class UserController {
 			}
 
 			if (notValid || user === null) {
-				const invalidCredential = new Error('Invalid email or password!');
-				invalidCredential.name = 'invalidCredential';
-				throw invalidCredential;
+				throw { name: 'invalidLogin' };
 			}
 		} catch (error) {
-			if (error.name === 'invalidCredential') {
-				return res.status(400).json({ message: error.message });
-			} else {
-				return res.status(500).json({ message: 'Internal server error' });
-			}
+			return next(error);
 		}
 	}
 }
