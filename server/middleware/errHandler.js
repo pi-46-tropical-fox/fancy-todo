25d59fb5f10061f99fb3ebc44657135783f21a4f
Eas <me@eas.web.id>
2020-09-02 23:45:01 +0700

    centralized error handling

--- server/middleware/errHandler.js
+++ server/middleware/errHandler.js
@@ -1,13 +1,48 @@
+const fs = require('fs')
+const logsPath = './logs'
+const dateNow = new Date(Date.now()).toISOString()
+const errorsPath = `${logsPath}/error-${dateNow.split('T')[0]}.log` // daily error log
+
 function errHandler(err, req, res, next) {
-    let msg = []
-    
-    if(Array.isArray(err.msg)){
-        msg = err.msg.map(message => message)
-    } else {
-        msg.push(err.msg)
+    let code = err.code || 500
+    let error = {
+        msg: [],
+        stack: err
     }
 
-    res.status(err.code).json({ msg })
+    console.log(err);
+
+    switch(err.name){
+        case 'SequelizeValidationError':
+            err.errors.forEach(e => {
+                error.msg.push(`${e.path}: ${e.message}`)
+            })
+        break
+        case 'SequelizeUniqueConstraintError':
+            err.errors.forEach(e => {
+                error.msg.push(`${e.type}: ${e.message}`)
+            })
+        break
+        case 'JsonWebTokenError':
+        default:
+            error.msg.push(`Oops! Something wrong happened on our end. Don't worry, we'll get this thing done fast.`)
+        break
+    }
+
+    let errorMsg = `${dateNow} - ${err}\n`
+    fs.appendFileSync(errorsPath, errorMsg)
+
+    if(false) delete error.stack
+             
+    
+    // if(Array.isArray(err.msg)){
+    //     msg = err.msg.map(message => message)
+    // } else {
+    //     msg.push(err.msg)
+    // }
+    
+
+    res.status(code).json(error)
 }
 
 module.exports = errHandler
