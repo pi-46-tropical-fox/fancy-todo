a70b6ddcff391c9517de7a514e8b17530939d660
FaudzanAdzima <faudzanad@gmail.com>
2020-09-02 16:47:22 +0700

    3rd Party API

--- server/routes/index.js
+++ server/routes/index.js
@@ -2,12 +2,15 @@ const route = require('express').Router()
 const todoRoute = require('./todoRoute')
 const userRoute = require('./userRoute')
 const { authentication, authorization } = require('../middlewares/auth')
+const RestoController = require('../controllers/RestoController')
 
 route.get('/', (req, res) => {
     res.send('Home')
 })
-route.use('/user', userRoute)
 
+route.use('/user', userRoute)
 route.use('/todos',authentication, todoRoute)
+
+route.get('/resto-recomendation', RestoController.recomendation)
 
 module.exports = route
