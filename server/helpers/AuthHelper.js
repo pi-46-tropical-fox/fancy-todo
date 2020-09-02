aab32dbe7f436b65278ee9efa3d175ad2f81c3d0
Eas <me@eas.web.id>
2020-09-02 12:27:08 +0700

    move auth to middleware

--- server/helpers/AuthHelper.js
+++ server/helpers/AuthHelper.js
@@ -1,5 +1,6 @@
 const jwt = require('jsonwebtoken')
 const errors = require('./ErrorHelper')
+const { User } = require('../models')
 const secret = process.env.SECRET
 
 const generateToken = (data) => {
@@ -10,37 +11,7 @@ const verifyToken = (data) => {
     return jwt.verify(data, secret)
 }
 
-const authenticate = (req, res, next) => {
-    const { access_token } = req.headers
-
-    
-    try {
-        req.userData = verifyToken(access_token)
-
-        next()
-    } catch (e) {
-        console.log(e);
-        return errors.throwUnauthenticated(res, `You either are not logged in or don't have proper access token.`)
-    }
-}
-
-// const authorize = (req, res, next) => {
-//     const { access_token } = req.headers
-    
-//     try {
-//         // 
-//     } catch (e) {
-//         return errors.throwUnauthorized(res, `You're not allowed to access this area`)
-//         res.status(403).json({ msg: '403 Unauthenticated'})
-//     }
-// }
-
 module.exports = {
-    // any levels
     generateToken,
     verifyToken,
-
-    // middleware-level
-    authenticate,
-    // authorize,
 }
