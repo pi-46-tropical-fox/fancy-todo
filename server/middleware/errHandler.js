765f6c2097019b37563625839af6d5860e54e5f3
Eas <me@eas.web.id>
2020-09-02 23:53:10 +0700

    better error handling add new error conditions fixed debug conditions

--- server/middleware/errHandler.js
+++ server/middleware/errHandler.js
@@ -11,28 +11,30 @@ function errHandler(err, req, res, next) {
     }
 
     console.log(err);
-
-    switch(err.name){
-        case 'SequelizeValidationError':
-            err.errors.forEach(e => {
-                error.msg.push(`${e.path}: ${e.message}`)
-            })
-        break
-        case 'SequelizeUniqueConstraintError':
-            err.errors.forEach(e => {
-                error.msg.push(`${e.type}: ${e.message}`)
-            })
-        break
-        case 'JsonWebTokenError':
-        default:
-            error.msg.push(`Oops! Something wrong happened on our end. Don't worry, we'll get this thing done fast.`)
-        break
+    if(err.name){
+        switch(err.name){
+            case 'SequelizeValidationError':
+                err.errors.forEach(e => {
+                    error.msg.push(`${e.path}: ${e.message}`)
+                })
+            break
+            case 'SequelizeUniqueConstraintError':
+                err.errors.forEach(e => {
+                    error.msg.push(`${e.type}: ${e.message}`)
+                })
+            break
+            case 'JsonWebTokenError':
+            default:
+                error.msg.push(`Oops! Something wrong happened on our end. Don't worry, we'll get this thing done fast.`)
+            break
+        }
+    } else if(err.code) {
+        error.msg.push(err.msg)
     }
-
     let errorMsg = `${dateNow} - ${err}\n`
     fs.appendFileSync(errorsPath, errorMsg)
 
-    if(false) delete error.stack
+    if(!process.env.DEBUG) delete error.stack
              
     
     // if(Array.isArray(err.msg)){
