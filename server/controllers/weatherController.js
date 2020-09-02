f3fea20c8677d981f069bb46d0bbd4849d7b770f
syukurbaru <syukurbaru@gmail.com>
2020-09-02 21:15:27 +0700

    Memperbaiki weather agar kotanya dinamis

--- server/controllers/weatherController.js
+++ server/controllers/weatherController.js
@@ -3,12 +3,14 @@ const axios = require('axios')
 class weatherController {
     static async showAll(req,res) {
         try {
-          const response = await axios.get(`http://api.weatherstack.com/current?access_key=${process.env.ACCESS_KEY}&query=Bogor`);
-        
-          res.status(200).json(response.data)
+          const city = req.query.query
+
+          const response = await axios.get(`http://api.weatherstack.com/current?access_key=${process.env.ACCESS_KEY}&query=${city}`);
+          res.send(response.data)
+          // res.status(200).json(response.data)
         } catch (error) {
-        
-          res.status(500).json({message: `Internal server error`})
+          res.send(error)
+          // res.status(500).json({message: `Internal server error`})
         }
       }    
 }
