1126d407c4d5e19b319da76f3d4453e5e1f7b354
parasaraib <manikparasara@ymail.com>
2020-09-04 13:17:04 +0800

    add update and delete

--- client/main.js
+++ client/main.js
@@ -25,6 +25,18 @@ function addMenu(event) {
   $("#form-edit-todo").hide();
 }
 
+let editId = 0; 
+function editMenu(event) {
+  editId = event.srcElement.dataset.id;
+  $("#form-login").hide();
+  $("#form-register").hide();
+  $("#form-add-todo").hide();
+  $("#todo-list").hide();
+  $("#form-todo-list").hide();
+  $("#form-edit-todo").show();
+}
+
+
 function listMenu(event) {
   $("#form-login").hide();
   $("#form-register").hide();
@@ -54,8 +66,8 @@ function listMenu(event) {
                 <h6 class="card-text">Due Date: ${element.due_date.split("T")[0]}</h6>
                 <p class="card-text">${element.description}</p>
                 <p class="card-text">Status: ${element.status ? "Finished" : "Not Yet"}</p>
-                <a class="btn btn-dark" href="#" id="card-edit">Edit</a>
-                <a class="btn btn-dark" href="#" id="card-delete">Delete</a>
+                <a class="btn btn-dark " href="#" id="card-edit" onclick="editMenu(event)" data-id = "${element.id}">Edit</a>
+                <a class="btn btn-dark" href="#" onclick="deleteTodo(event)" data-id = "${element.id}">Delete</a>
               </div>
             </div>
           </div>
@@ -86,9 +98,13 @@ function initContent(event) {
   $("#todo-list").hide();
   $("#form-todo-list").hide();
   $("#form-edit-todo").hide();
+
+  $("#weather-forecast").empty();
+  getWeather();
 }
 
 function beforeLogin() {
+  $("#errorAlert").hide();
   $("#nav-login").show();
   $("#nav-register").show();
   $("#nav-home").hide();
@@ -98,6 +114,7 @@ function beforeLogin() {
 }
 
 function afterLogin() {
+  $("#errorAlert").hide();
   $("#nav-login").hide();
   $("#nav-register").hide();
   $("#nav-home").show();
@@ -128,7 +145,13 @@ function loginForm(event) {
       afterLogin();
     })
     .fail((err) => {
-      console.log(err);
+      console.log(err, "<<<< error in loginForm");
+      $("#errorAlert").show()
+      err.responseJSON.errors.forEach((error) => {
+        $("#errorAlert").append(`
+        <p>${error}</p>
+        `);
+      });
     })
 }
 
@@ -158,7 +181,13 @@ function registerForm(event) {
       afterLogin();
     })
     .fail((err) => {
-      console.log(err);
+      console.log(err, "<<<< errror in registerForm main");
+      $("#errorAlert").show();
+      err.responseJSON.errors.forEach((error) => {
+        $("#errorAlert").append(`
+        <p>${error}</p>
+        `);
+      });
     })
 }
 
@@ -184,7 +213,13 @@ function onSignIn(googleUser) {
       afterLogin();
     })
     .fail((err) => {
-      console.log(err);
+      console.log(err, "<<<< error in onSignIn");
+      $("#errorAlert").show();
+      err.responseJSON.errors.forEach((error) => {
+        $("#errorAlert").append(`
+        <p>${error}</p>
+        `);
+      });
     })
 }
 
@@ -195,6 +230,141 @@ function signOut() {
     console.log("User sign out");
   });
 }
+
+function getWeather() {
+  $.ajax({
+    method: "GET",
+    url: "http://localhost:3000/thirdparty/weather",
+  })
+    .done((response) => {
+      console.log(response);
+      $("#weather-forecast").append(`
+        <h5 class="card-title">Today's Weather</h5>
+        <h6 class="card-text">${response.location.localtime}<h6>
+        <h6 class="card-text">${response.request.query}<h6>
+        <h6 class="card-text">Temp: ${response.current.temperature}&#8451; ${response.current.weather_descriptions[0]}</h6>
+      `);
+    })
+    .fail((err) => {
+      console.log(err, "<<<< error in getWeather");
+      $("#errorAlert").show();
+      err.responseJSON.errors.forEach((error) => {
+        $("#errorAlert").append(`
+        <p>${error}</p>
+        `);
+      });
+    })
+}
+
+function addTodo() {
+  event.preventDefault();
+  const title = $("#addTitle").val();
+  const description = $("#addDescription").val();
+  const due_date = $("#addDueDate").val();
+
+  $.ajax({
+    method: "POST",
+    url: "http://localhost:3000/todos",
+    data: {
+      title,
+      description,
+      due_date
+    },
+    headers: {
+      access_token: localStorage.getItem("access_token")
+    }
+  })
+    .done((response) => {
+      console.log(response);
+
+      $("#addTitle").val("");
+      $("#addDescription").val("");
+      $("#addDueDate").val("");
+      listMenu();
+      afterLogin();
+    })
+    .fail((err) => {
+      console.log(err, "<<<< error in addTodo");
+      $("#errorAlert").show();
+      err.responseJSON.errors.forEach((error) => {
+        $("#errorAlert").append(`
+        <p>${error}</p>
+        `);
+      });
+    })
+}
+
+function editTodo() {
+  const id = editId;
+  event.preventDefault();
+  const title = $("#editTitle").val();
+  const description = $("#editDescription").val();
+  const status = $("input[name=inlineRadioOptions]").value;
+  const due_date = $("#editDueDate").val();
+
+  $.ajax({
+    method: "PUT",
+    url: `http://localhost:3000/todos/${id}`,
+    data: {
+      title,
+      description,
+      status,
+      due_date
+    },
+    headers: {
+      access_token: localStorage.getItem("access_token")
+    }
+  })
+    .done((response) => {
+      console.log(response);
+
+      $("#editTitle").val("");
+      $("#editDescription").val("");
+      $("#editDueDate").val("");
+      listMenu();
+      afterLogin();
+    })
+    .fail((err) => {
+      console.log(err, "<<<< error in editTodo");
+      $("#errorAlert").show();
+      err.responseJSON.errors.forEach((error) => {
+        $("#errorAlert").append(`
+        <p>${error}</p>
+        `);
+      });
+    })
+}
+
+function deleteTodo(event) {
+  console.log(event.srcElement.dataset.id);
+  const id = event.srcElement.dataset.id;
+  event.preventDefault();
+
+  $.ajax({
+    method: "DELETE",
+    url: `http://localhost:3000/todos/${id}`,
+    headers: {
+      access_token: localStorage.getItem("access_token")
+    }
+  })
+    .done((response) => {
+      console.log(response);
+
+      listMenu();
+      afterLogin();
+    })
+    .fail((err) => {
+      console.log(err, "<<<< error in deleteTodo");
+      $("#errorAlert").show();
+      err.responseJSON.errors.forEach((error) => {
+        $("#errorAlert").append(`
+        <p>${error}</p>
+        `);
+      });
+    })
+}
+
+
 
 $(document).ready(() => {
   initContent();
@@ -213,8 +383,14 @@ $(document).ready(() => {
   $("#nav-add").click(addMenu);
   $("#nav-logout").click(logoutMenu);
 
+  $("#card-edit").click(editMenu);
+
   $("#loginForm").submit(loginForm);
 
   $("#registerForm").submit(registerForm);
+
+  $("#addForm").submit(addTodo);
+
+  $("#editForm").submit(editTodo);
 
 });
