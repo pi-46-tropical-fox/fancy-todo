0a96c450281c538de636e16399dbc4b1a7995527
Eas <me@eas.web.id>
2020-09-05 17:42:43 +0700

    add Google OAuth login

--- server/controllers/AuthController.js
+++ server/controllers/AuthController.js
@@ -1,6 +1,7 @@
 const { User } = require('../models')
 const { generateToken } = require('../helpers/AuthHelper')
-const { compareHash } = require('../helpers/BcryptHelper')
+const { compareHash, generateRandomPassword } = require('../helpers/BcryptHelper')
+const { OAuth2Client } = require('google-auth-library')
 
 class AuthController {
     static async login(req, res, next){
@@ -12,8 +13,10 @@ class AuthController {
                 let valid = compareHash(password, user.password)
                 if(valid){
                     let access_token = generateToken({ username, id: user.id })
+                    let name = user.name
+                    username = user.username
 
-                    return res.status(200).json(access_token)
+                    return res.status(200).json({ access_token, name, username })
                 }else{
                     throw { code: 400, msg: 'Username or password you entered is incorrect. Hmm... which one is it?' }
                 }
@@ -22,6 +25,47 @@ class AuthController {
             }
         } catch (err) {
             return next(err)
+        }
+    }
+
+    static async googleLogin(req, res, next){
+        try{
+            let code = 200
+            const client = new OAuth2Client(process.env.GOOGLE_CLIENT_ID)
+            const { google_access_token } = req.headers
+
+            const ticket = await client.verifyIdToken({
+                idToken: google_access_token,
+                audience: process.env.GOOGLE_CLIENT_ID
+            })
+
+            const payload = ticket.getPayload()
+
+            let user = await User.findOne({ where: { username: payload.email }})
+
+            if(!user){
+                const newUser = {
+                    name: payload.name,
+                    username: payload.email,
+                    password: generateRandomPassword(20)
+                }
+
+                user = await User.create(newUser)
+
+                code = 201
+            }
+
+            let tokenData = { id: user.id, username: user.username }
+
+            const data = {
+                access_token: generateToken(tokenData),
+                name: user.name,
+                username: user.username
+            }
+
+            res.status(code).json(data)
+        }catch(err){
+            next(err)
         }
     }
 
--- server/routes/endpoints/auth.js
+++ server/routes/endpoints/auth.js
@@ -5,5 +5,6 @@ auth
 // .get('/login', AuthController.loginForm)
 .post('/login', AuthController.login)
 .post('/register', AuthController.register)
+.post('/googleLogin', AuthController.googleLogin)
 
 module.exports = auth
