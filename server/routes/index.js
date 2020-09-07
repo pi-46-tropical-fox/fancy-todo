89a71d4e07a542b81a78ec02fbfaf4e6b75c61c8
adiet95 <diezzyshop@gmail.com>
2020-09-07 23:15:32 +0700

    Testing Heroku

--- server/routes/index.js
+++ server/routes/index.js
@@ -4,9 +4,9 @@ const TodoRoute = require('./TodoRoute')
 const authorization = require('../middleware/authorization')
 const authentication = require('../middleware/authentication')
 
-router.get('/', (req, res, next) => {
-    res.json({message:'Welcome to My Apps'})
-})
+// router.get('/', (req, res, next) => {
+//     res.json({message:'Welcome to My Apps'})
+// })
 
 router.use('/', UserRoute)
 router.use('/todos', authentication, authorization, TodoRoute)
