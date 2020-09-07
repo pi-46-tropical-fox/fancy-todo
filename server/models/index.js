ef4208e92398c18784deea82103d994f6cc25a55
mohammad rendra <abuabdillatief@gmail.com>
2020-09-07 14:22:02 +0700

    setting db heroku

--- server/models/index.js
+++ server/models/index.js
@@ -4,13 +4,14 @@ const fs = require('fs');
 const path = require('path');
 const Sequelize = require('sequelize');
 const basename = path.basename(__filename);
-const env = process.env.NODE_ENV || 'development';
+const env = process.env.NODE_ENV || 'development'; //production
 const config = require(__dirname + '/../config/config.json')[env];
 const db = {};
 
 let sequelize;
 if (config.use_env_variable) {
     sequelize = new Sequelize(process.env[config.use_env_variable], config);
+    //process.env['DARABASE_URL']
 } else {
     sequelize = new Sequelize(config.database, config.username, config.password, config);
 }
