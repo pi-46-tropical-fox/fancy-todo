e739bf2cb125e10f8d7c365399e041a58500731d
Fandi Ario <fandi.ario10@gmail.com>
2020-09-07 22:48:34 +0700

    Release 7.2 -- First Try Deploy

--- server/routes/index.js
+++ server/routes/index.js
@@ -5,6 +5,10 @@ const TodoRoute = require ("./TodoRoute.js")
 const UserRoute = require ("./UserRoute.js")
 const CalendarRoute = require ("./CalendarRoute.js")
 
+router.get ("/", (req, res, next) => {
+    res.json ({message : "Welcome to fancy-todo"})
+})
+
 
 router.use ("/todos", TodoRoute)
 router.use ("/users", UserRoute)
