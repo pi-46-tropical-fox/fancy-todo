2d04993c5f9d60561c2383dcd1e66cbe96714e74
juanbrundism <juanbrundism@gmail.com>
2020-09-08 00:57:33 +0700

    Add __driname to app.js

--- server/app.js
+++ server/app.js
@@ -4,6 +4,7 @@ if(process.env.NODE_ENV === "development"){
 
 const express = require(`express`)
 const cors = require(`cors`)
+const distDir = __dirname + "/dist/";
 
 const routes = require(`./routes`)
 const {errHandler} = require(`./middlewares`)
@@ -12,6 +13,7 @@ const app = express()
 const PORT = process.env.PORT || 3000
 
 app.use(cors())
+app.use(express.static(distDir));
 app.use(express.urlencoded( { extended: true } ))
 app.use(express.json())
 
