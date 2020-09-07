e88ea89dc5b6bf7c077b9c801af7b8962296a363
adiet95 <diezzyshop@gmail.com>
2020-09-07 22:12:29 +0700

    Prepare deploy Server Heroku

--- server/app.js
+++ server/app.js
@@ -1,8 +1,10 @@
-require('dotenv').config()
+if(process.env.NODE_ENV === 'development'){
+    require('dotenv').config()
+}
 const express = require('express')
 const app = express()
 var cors = require('cors')
-const port = 3000
+const port = process.env.PORT || 3000
 const routes = require('./routes')
 const errorHandler = require('./middleware/errorHandler')
 
