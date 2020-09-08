ceee9758db85bd7a923a58a99b9e04a7ccc2ae1e
Brando Juberd <brandowalandouw@gmail.com>
2020-09-08 09:28:59 +0700

    heroku deploy fixes

--- server/app.js
+++ server/app.js
@@ -3,7 +3,7 @@ if(process.env.NODE_ENV === "deployment"){
 }
 const express = require('express')
 const app = express()
-const port = 3000
+const port = process.env.PORT||3000
 const router = require("./routes")
 const errorHandler = require("./middlewares/errorHandler")
 const cors = require("cors")
