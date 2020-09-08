2a7d188f340a497abc374442e739157ca3ce7e65
juanbrundism <juanbrundism@gmail.com>
2020-09-08 16:51:57 +0700

    edit env app.js

--- server/app.js
+++ server/app.js
@@ -1,6 +1,7 @@
-if(process.env.NODE_ENV === "development"){
+if(process.env.NODE_ENV === "development" || !process.env.NODE_ENV){
     require('dotenv').config()
 }
+
 
 const express = require(`express`)
 const cors = require(`cors`)
