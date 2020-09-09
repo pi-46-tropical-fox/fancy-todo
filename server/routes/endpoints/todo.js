174eedca99739569721b91c135eaea096f854600
Eas <me@eas.web.id>
2020-09-09 20:21:44 +0700

    add authorize middleware

--- server/routes/endpoints/todo.js
+++ server/routes/endpoints/todo.js
@@ -1,11 +1,12 @@
 const todo = require('express').Router()
 const TodoController = require('../../controllers/TodoController')
+const { authorize } = require('../../middleware/authHandler')
 
 todo
 .get('/', TodoController.readAll)
-.get('/:id', TodoController.readOne)
+.get('/:id', authorize, TodoController.readOne)
 .post('/', TodoController.create)
-.put('/:id', TodoController.update)
-.delete('/:id', TodoController.delete)
+.put('/:id', authorize, TodoController.update)
+.delete('/:id', authorize, TodoController.delete)
 
 module.exports = todo
