b4e6741020910d19a532f9b18f79a14dd4f16ebf
Eas <me@eas.web.id>
2020-09-09 21:17:28 +0700

    typo fix

--- client/assets/js/main.js
+++ client/assets/js/main.js
@@ -1,4 +1,4 @@
-const host = 'https://todoers-app.herokuapp.com/'
+const host = 'https://todoers-app.herokuapp.com'
 // const host = 'http://localhost'
 const port = null
 const baseUrl = `${host}${ port ? `:${port}` : '' }`
