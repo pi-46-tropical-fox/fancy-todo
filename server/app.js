8b460c26b195e7947e5fdeec9e8d466da85f53ba
Eas <me@eas.web.id>
2020-09-09 20:21:37 +0700

    add shell command

--- server/app.js
+++ server/app.js
@@ -1,24 +1,28 @@
-if(process.env.NODE_ENV === 'development') require('dotenv').config()
+const { exec } = require('child_process')
 
-const express = require('express')
-const routes = require('./routes')
-const errHandler = require('./middleware/errHandler')
-const app = express()
-const cors = require('cors')
-const port = process.env.PORT || 3457
+exec(`npm ls dotenv`, (err, stdout, stderr) => { // checks for dotenv extension
+    if(stdout.search('empty') === -1) require('dotenv').config()
 
-// Trying the method chaining for the first time...
+    const express = require('express')
+    const routes = require('./routes')
+    const errHandler = require('./middleware/errHandler')
+    const app = express()
+    const cors = require('cors')
+    const port = process.env.PORT || 3457
 
-app
-// first, use needed modules for processing requests
-.use(express.urlencoded({ extended: false }))
-.use(express.json())
-.use(cors())
-// ...and then import needed routes
-.use(routes)
-.use(errHandler)
-// ...and lastly, listen to specified port
-.listen(port, () => {
-    // voila! The app is up and running.
-    console.log(`The app is up and running. Tune into http://localhost:${port} to proceed.`);
+    // Trying the method chaining for the first time...
+
+    app
+    // first, use needed modules for processing requests
+    .use(express.urlencoded({ extended: false }))
+    .use(express.json())
+    .use(cors())
+    // ...and then import needed routes
+    .use(routes)
+    .use(errHandler)
+    // ...and lastly, listen to specified port
+    .listen(port, () => {
+        // voila! The app is up and running.
+        console.log(`The app is up and running. Tune into http://localhost:${port} to proceed.`);
+    })
 })
